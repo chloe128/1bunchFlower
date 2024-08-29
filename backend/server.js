4035b5520eac7c86d5350661d72602c006e34c20
chloe128 <chloemeng98@gmail.com>
2024-08-29 04:05:39 -0700

    deveploy

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 //cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running..");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running..");
+// });
 
 app.use("/api/products", productRoutes);
 app.use("/api/user", userRoutes);
@@ -29,6 +29,20 @@ app.use("/api/orders", orderRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
+
+//prod
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
